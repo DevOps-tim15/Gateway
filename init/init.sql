b7047b11776af3754c4c03e631fe5dfd091414a6
nikolina97 <41080295+nikolina97@users.noreply.github.com>
2021-09-08 22:27:13 +0200

    Gateway routes for posts

--- init/init.sql
+++ init/init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE userauth;
 CREATE DATABASE userservice;
+CREATE DATABASE postservice;
 
 # create root user and grant rights
 CREATE USER 'sa'@'localhost' IDENTIFIED BY 'password';
@@ -27,6 +28,19 @@ PRIMARY KEY (id)
 );
 
 USE `userservice`;
+INSERT INTO `authorities` (`id`,`user_type`) VALUES ('1','ROLE_REGISTERED_USER');
+INSERT INTO `authorities` (`id`,`user_type`) VALUES ('2','ROLE_AGENT');
+INSERT INTO `authorities` (`id`,`user_type`) VALUES ('3','ROLE_ADMIN');
+
+USE `postservice`;
+CREATE TABLE IF NOT EXISTS authorities
+(
+`id` INTEGER AUTO_INCREMENT,
+`user_type` VARCHAR(255),
+PRIMARY KEY (id)
+);
+
+USE `postservice`;
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('1','ROLE_REGISTERED_USER');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('2','ROLE_AGENT');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('3','ROLE_ADMIN');
