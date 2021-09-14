2521b3c397a17876fd688ef06d3539947cd5ccaa
nikolina97 <41080295+nikolina97@users.noreply.github.com>
2021-09-14 15:16:05 +0200

    Release 2
    * Gateway for registration added
    
    * Kafka added
    
    * Gateway routes for posts
    
    * admin added
    
    Co-authored-by: kaca997 <katarina.aleksic97@gmail.com>

--- init/init.sql
+++ init/init.sql
@@ -15,9 +15,52 @@ PRIMARY KEY (id)
 );
 
 USE `userauth`;
+CREATE TABLE IF NOT EXISTS user_t
+(
+`id` INTEGER AUTO_INCREMENT,
+`first_name` VARCHAR(255),
+`last_name` VARCHAR(255),
+`email` VARCHAR(255),
+`username` VARCHAR(255),
+`password` VARCHAR(255),
+`phone` VARCHAR(255),
+`website_url` VARCHAR(255),
+`sex` VARCHAR(255),
+`birth_date` VARCHAR(255),
+`biography` VARCHAR(255),
+`verified` BIT(1),
+`can_be_tagged` BIT(1),
+`is_private` BIT(1),
+PRIMARY KEY (id)
+);
+
+USE `userauth`;
+CREATE TABLE IF NOT EXISTS user_authority
+(
+`user_id` INTEGER,
+`authority_id` INTEGER,
+FOREIGN KEY (user_id) REFERENCES user_t(id),
+FOREIGN KEY (authority_id) REFERENCES authorities(id)
+);
+
+USE `userauth`;
+CREATE TABLE IF NOT EXISTS verification_tokens
+(
+`id` INTEGER AUTO_INCREMENT,
+`token` VARCHAR(255),
+`user_id` INTEGER,
+PRIMARY KEY (id),
+FOREIGN KEY (user_id) REFERENCES user_t(id)
+);
+
+USE `userauth`;
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('1','ROLE_REGISTERED_USER');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('2','ROLE_AGENT');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('3','ROLE_ADMIN');
+INSERT INTO `user_t` (`id`,`first_name`, `last_name`, `email`, `username`, `password`, `phone`, `website_url`, `sex`, `birth_date`, `biography`, `verified`, `can_be_tagged`, `is_private`) 
+VALUES ('5','Jova', 'Jovic', 'jova.jovic@gmail.com', 'jova', '$2a$12$ix4Ep6eG2ajt5yjWpAlRHusH1srR8GXdh0FvrgRWnVv2hZVRWEhoC', '0601234567', 'somesite.com', 'male', '01.01.01.', 'bio', 1, 1, 0);
+INSERT INTO user_authority (user_id, authority_id) VALUES (5, 3);
+INSERT INTO verification_tokens (`id`, `token`, `user_id`) VALUES (5, 'joca-token', 5);
 
 USE `userservice`;
 CREATE TABLE IF NOT EXISTS authorities
@@ -28,9 +71,52 @@ PRIMARY KEY (id)
 );
 
 USE `userservice`;
+CREATE TABLE IF NOT EXISTS user_t
+(
+`id` INTEGER AUTO_INCREMENT,
+`first_name` VARCHAR(255),
+`last_name` VARCHAR(255),
+`email` VARCHAR(255),
+`username` VARCHAR(255),
+`password` VARCHAR(255),
+`phone` VARCHAR(255),
+`website_url` VARCHAR(255),
+`sex` VARCHAR(255),
+`birth_date` VARCHAR(255),
+`biography` VARCHAR(255),
+`verified` BIT(1),
+`can_be_tagged` BIT(1),
+`is_private` BIT(1),
+PRIMARY KEY (id)
+);
+
+USE `userservice`;
+CREATE TABLE IF NOT EXISTS user_authority
+(
+`user_id` INTEGER,
+`authority_id` INTEGER,
+FOREIGN KEY (user_id) REFERENCES user_t(id),
+FOREIGN KEY (authority_id) REFERENCES authorities(id)
+);
+
+USE `userservice`;
+CREATE TABLE IF NOT EXISTS verification_tokens
+(
+`id` INTEGER AUTO_INCREMENT,
+`token` VARCHAR(255),
+`user_id` INTEGER,
+PRIMARY KEY (id),
+FOREIGN KEY (user_id) REFERENCES user_t(id)
+);
+
+USE `userservice`;
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('1','ROLE_REGISTERED_USER');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('2','ROLE_AGENT');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('3','ROLE_ADMIN');
+INSERT INTO `user_t` (`id`,`first_name`, `last_name`, `email`, `username`, `password`, `phone`, `website_url`, `sex`, `birth_date`, `biography`, `verified`, `can_be_tagged`, `is_private`) 
+VALUES ('5','Jova', 'Jovic', 'jova.jovic@gmail.com', 'jova', '$2a$12$ix4Ep6eG2ajt5yjWpAlRHusH1srR8GXdh0FvrgRWnVv2hZVRWEhoC', '0601234567', 'somesite.com', 'male', '01.01.01.', 'bio', 1, 1, 0);
+INSERT INTO user_authority (user_id, authority_id) VALUES (5, 3);
+INSERT INTO verification_tokens (`id`, `token`, `user_id`) VALUES (5, 'joca-token', 5);
 
 USE `postservice`;
 CREATE TABLE IF NOT EXISTS authorities
@@ -41,6 +127,49 @@ PRIMARY KEY (id)
 );
 
 USE `postservice`;
+CREATE TABLE IF NOT EXISTS user_t
+(
+`id` INTEGER AUTO_INCREMENT,
+`first_name` VARCHAR(255),
+`last_name` VARCHAR(255),
+`email` VARCHAR(255),
+`username` VARCHAR(255),
+`password` VARCHAR(255),
+`phone` VARCHAR(255),
+`website_url` VARCHAR(255),
+`sex` VARCHAR(255),
+`birth_date` VARCHAR(255),
+`biography` VARCHAR(255),
+`verified` BIT(1),
+`can_be_tagged` BIT(1),
+`is_private` BIT(1),
+PRIMARY KEY (id)
+);
+
+USE `postservice`;
+CREATE TABLE IF NOT EXISTS user_authority
+(
+`user_id` INTEGER,
+`authority_id` INTEGER,
+FOREIGN KEY (user_id) REFERENCES user_t(id),
+FOREIGN KEY (authority_id) REFERENCES authorities(id)
+);
+
+USE `postservice`;
+CREATE TABLE IF NOT EXISTS verification_tokens
+(
+`id` INTEGER AUTO_INCREMENT,
+`token` VARCHAR(255),
+`user_id` INTEGER,
+PRIMARY KEY (id),
+FOREIGN KEY (user_id) REFERENCES user_t(id)
+);
+
+USE `postservice`;
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('1','ROLE_REGISTERED_USER');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('2','ROLE_AGENT');
 INSERT INTO `authorities` (`id`,`user_type`) VALUES ('3','ROLE_ADMIN');
+INSERT INTO `user_t` (`id`,`first_name`, `last_name`, `email`, `username`, `password`, `phone`, `website_url`, `sex`, `birth_date`, `biography`, `verified`, `can_be_tagged`, `is_private`) 
+VALUES ('5','Jova', 'Jovic', 'jova.jovic@gmail.com', 'jova', '$2a$12$ix4Ep6eG2ajt5yjWpAlRHusH1srR8GXdh0FvrgRWnVv2hZVRWEhoC', '0601234567', 'somesite.com', 'male', '01.01.01.', 'bio', 1, 1, 0);
+INSERT INTO user_authority (user_id, authority_id) VALUES (5, 3);
+INSERT INTO verification_tokens (`id`, `token`, `user_id`) VALUES (5, 'joca-token', 5);
